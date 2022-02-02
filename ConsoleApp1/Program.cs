60972b447a7da45760df356f3ce7b01f5b56a9e1
Jonathan Hop <yithril@gmail.com>
2022-02-01 21:23:28 -0500

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's the length of the room?");
+            string length = Console.ReadLine();
+            Console.WriteLine("What's the width of the room?");
+
+            //I feel like using var now.
+            var width = Console.ReadLine();
+
+            double parsedLength = Convert.ToDouble(length);
+
+            var parsedWidth = double.Parse(width);
+
+            double perimeter = 2 * parsedLength + 2 * parsedWidth;
+
+            var area = parsedWidth * parsedLength;
+
+            Console.WriteLine("The area is " + area);
+            Console.WriteLine("The perimeter is " + perimeter);
+        }
+    }
+}
